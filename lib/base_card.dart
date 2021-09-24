9d1246d77e485f6cb0eeefff6f1362b54deb82f9
陈喆 <307400468@qq.com>
2021-09-24 21:35:48 +0800

    卡片基类

---
+++ lib/base_card.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class BaseCard extends StatefulWidget {
+  const BaseCard({ Key? key }) : super(key: key);
+
+  @override
+  BaseCardState createState() => BaseCardState();
+}
+
+class BaseCardState extends State<BaseCard> {
+  Color subTitleColor = Colors.grey;
+  Color bottomTitleColor = Colors.grey;
+  @override
+  Widget build(BuildContext context) {
+    // 裁切圆角
+    return PhysicalModel(
+      color: Colors.transparent,
+      borderRadius: BorderRadius.circular(6),
+      clipBehavior: Clip.antiAlias,// 抗锯齿
+      child: Container(
+        decoration: BoxDecoration(color: Colors.white),
+        child: Column(
+          children: [
+            topContent(),
+            bottomContent()
+          ],
+        ),
+      )
+    );
+  }
+
+  topContent() {
+    return Padding(
+      padding: EdgeInsets.only(left: 20, top: 26, bottom: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              topTitle(''),
+              topTitle2('')
+            ]
+          ),
+          subTitle('')
+        ],
+      ),
+    );
+  }
+
+  bottomContent() {
+    return Container();
+  }
+
+  Widget subTitle(String title) {
+    return Padding(
+      padding: EdgeInsets.only(top: 5),
+      child: Text(title, style: TextStyle(fontSize: 11, color: subTitleColor))
+    );
+  }
+
+  topTitle(String title) {
+    return Text(title, style: TextStyle(fontSize: 22),);
+  }
+  topTitle2(String title) {
+    return Container();
+  }
+
+  bottomTitle(String title) {
+    return Text(
+      title,
+      textAlign: TextAlign.center,
+      style: TextStyle(fontSize: 12, color: bottomTitleColor),
+    );
+  }
+}
